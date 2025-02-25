c79c6ba19458c79b09200ba2f4993dd534729498
Vulcan_HP_Visual_Ubuntu <2912679550@qq.com>
2025-02-25 15:55:58 +0800

    创建仓库版本

---
+++ src/homework1-3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+
+using namespace std;
+
+int main(int argc , char **argv){
+    // todo 初始化小变量w
+    Eigen::Vector3d w(0.01, 0.02, 0.03); // 小量角速度
+}
+
+
+
+
+
+
+
+
+
+
+
