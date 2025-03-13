b0d73999476b84d8e37e4d297c879b623bb6054e
vulcan-HP-Ubuntu <zhiyuanwu@hust.edu.cn>
2025-03-13 13:52:26 +0800

    250313初版编译通过，但运行还有CV报错

--- vio_hw7/test/run_euroc.cpp
+++ vio_hw7/test/run_euroc.cpp
@@ -7,9 +7,9 @@
 #include <thread>
 #include <iomanip>
 
-#include <cv.h>
+
 #include <opencv2/opencv.hpp>
-#include <highgui.h>
+#include <opencv2/highgui/highgui.hpp>
 #include <eigen3/Eigen/Dense>
 #include "System.h"
 
@@ -18,8 +18,10 @@ using namespace cv;
 using namespace Eigen;
 
 const int nDelayTimes = 2;
-string sData_path = "/home/dataset/EuRoC/MH-05/mav0/";
-string sConfig_path = "../config/";
+string sData_path = "/home/vulcan/data_bag/MH_05_difficult/mav0/";
+// string sConfig_path = "../config/";
+string sPackage_path = "/home/vulcan/Robot_locate/VIO_class/src/Homework_vio_gaoxiang/vio_hw7/";
+string sConfig_path = "/home/vulcan/Robot_locate/VIO_class/src/Homework_vio_gaoxiang/vio_hw7/config/";
 
 std::shared_ptr<System> pSystem;
 
@@ -68,7 +70,7 @@ void PubImageData()
 	double dStampNSec;
 	string sImgFileName;
 	
-	// cv::namedWindow("SOURCE IMAGE", CV_WINDOW_AUTOSIZE);
+	// cv::namedWindow("SOURCE IMAGE", cv::WINDOW_AUTOSIZE);
 	while (std::getline(fsImage, sImage_line) && !sImage_line.empty())
 	{
 		std::istringstream ssImuData(sImage_line);
@@ -90,88 +92,15 @@ void PubImageData()
 	fsImage.close();
 }
 
-#ifdef __APPLE__
-// support for MacOS
-void DrawIMGandGLinMainThrd(){
-	string sImage_file = sConfig_path + "MH_05_cam0.txt";
-
-	cout << "1 PubImageData start sImage_file: " << sImage_file << endl;
-
-	ifstream fsImage;
-	fsImage.open(sImage_file.c_str());
-	if (!fsImage.is_open())
-	{
-		cerr << "Failed to open image file! " << sImage_file << endl;
-		return;
-	}
-
-	std::string sImage_line;
-	double dStampNSec;
-	string sImgFileName;
-
-	pSystem->InitDrawGL();
-	while (std::getline(fsImage, sImage_line) && !sImage_line.empty())
-	{
-		std::istringstream ssImuData(sImage_line);
-		ssImuData >> dStampNSec >> sImgFileName;
-		// cout << "Image t : " << fixed << dStampNSec << " Name: " << sImgFileName << endl;
-		string imagePath = sData_path + "cam0/data/" + sImgFileName;
-
-		Mat img = imread(imagePath.c_str(), 0);
-		if (img.empty())
-		{
-			cerr << "image is empty! path: " << imagePath << endl;
-			return;
-		}
-		//pSystem->PubImageData(dStampNSec / 1e9, img);
-		cv::Mat show_img;
-		cv::cvtColor(img, show_img, CV_GRAY2RGB);
-		if (SHOW_TRACK)
-		{
-			for (unsigned int j = 0; j < pSystem->trackerData[0].cur_pts.size(); j++)
-			{
-				double len = min(1.0, 1.0 *  pSystem->trackerData[0].track_cnt[j] / WINDOW_SIZE);
-				cv::circle(show_img,  pSystem->trackerData[0].cur_pts[j], 2, cv::Scalar(255 * (1 - len), 0, 255 * len), 2);
-			}
-
-			cv::namedWindow("IMAGE", CV_WINDOW_AUTOSIZE);
-			cv::imshow("IMAGE", show_img);
-		  // cv::waitKey(1);
-		}
-
-		pSystem->DrawGLFrame();
-		usleep(50000*nDelayTimes);
-	}
-	fsImage.close();
-
-} 
-#endif
-
 int main(int argc, char **argv)
 {
-	if(argc != 3)
-	{
-		cerr << "./run_euroc PATH_TO_FOLDER/MH-05/mav0 PATH_TO_CONFIG/config \n" 
-			<< "For example: ./run_euroc /home/stevencui/dataset/EuRoC/MH-05/mav0/ ../config/"<< endl;
-		return -1;
-	}
-	sData_path = argv[1];
-	sConfig_path = argv[2];
-
 	pSystem.reset(new System(sConfig_path));
 	
 	std::thread thd_BackEnd(&System::ProcessBackEnd, pSystem);
-		
 	// sleep(5);
 	std::thread thd_PubImuData(PubImuData);
-
 	std::thread thd_PubImageData(PubImageData);
-
-#ifdef __linux__	
 	std::thread thd_Draw(&System::Draw, pSystem);
-#elif __APPLE__
-	DrawIMGandGLinMainThrd();
-#endif
 
 	thd_PubImuData.join();
 	thd_PubImageData.join();
